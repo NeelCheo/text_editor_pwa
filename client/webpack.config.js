78a0c22ac90542e59d99a15f34b62dd654cc8353
NeelCheo <73446815+NeelCheo@users.noreply.github.com>
2023-05-17 19:57:27 -0400

    edite the files for the database and webpack config as well as the sourcing

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,58 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const path = require('path');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'PWA Text Editor',
+      short_name: 'Text Editor',
+      description: 'Text Editor made into a PWA',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      start_url: '/',
+      icons: [{
+        src: path.resolve('src/images/logo.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join('assets', 'icons')
+      }]
+    }),
+    new InjectManifest({
+      swSrc: './src/src-sw.js'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+  },
 };
